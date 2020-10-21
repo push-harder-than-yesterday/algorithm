c3e872057d8f9b02f0d77bbe0c3d429754643b2c
jong yoon <dhorlawhddbs@naver.com>
2020-10-21 18:18:05 +0900

    Serialize-and-Deserialize-Binary-Tree 문제 다시 풀기

--- week7/Serialize-and-Deserialize-Binary-Tree/jongnan.js
+++ week7/Serialize-and-Deserialize-Binary-Tree/jongnan.js
@@ -23,12 +23,26 @@
  */
 var serialize = function (root) {
 	//return JSON.stringify(root);
-	const arr = tree2Arr(root);
-	return arr.reduce((str, val, idx) => {
-		str += val === null ? 'null' : val;
-		if (idx !== arr.length - 1) str += ',';
-		return str;
-	}, '');
+	// const arr = tree2Arr(root);
+	// return arr.reduce((str, val, idx) => {
+	// 	str += val === null ? 'null' : val;
+	// 	if (idx !== arr.length - 1) str += ',';
+	// 	return str;
+	// }, '');
+	const str = convertTreeToStr(root, '');
+	console.log(str);
+};
+
+var convertTreeToStr = function (node, str) {
+	if (node === null) return '';
+	str += `{ val:${node.val}`;
+	if (node.left !== null) {
+		str += `,left:${convertTreeToStr(node.left, str)}`;
+	}
+	if (node.right !== null) {
+		str += `,right:${convertTreeToStr(node.right, str)}`;
+	}
+	return (str += '}');
 };
 
 var tree2Arr = function (root) {
@@ -95,7 +109,111 @@ var deserialize = function (data) {
 	return root;
 };
 
+//=========================================================
+// 접근 방식 2
+//---------------------------------------------------------
+// 트리 노드를 깊이 우선 탐색을 통해 스트링으로 변경
+// [1,2,3,null,null,4,5] ->
+//                  {v:1,l:{v:2},r:{v:3,l:{v:4},r:{v:5}}}
+// 이를 파싱하면서 다시 트리 노드로 변경
+// 파싱시에도 dfs 이용
+//=========================================================
 /**
- * Your functions will be called as such:
- * deserialize(serialize(root));
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
  */
+var serialize = function (root) {
+	return convertTreeToStr(root, '');
+};
+
+var convertTreeToStr = function (node, str) {
+	if (node === null) return '';
+	let nodeStr = `{v:${node.val}`;
+	if (node.left !== null) {
+		nodeStr += `,l:${convertTreeToStr(node.left, str)}`;
+	}
+	if (node.right !== null) {
+		nodeStr += `,r:${convertTreeToStr(node.right, str)}`;
+	}
+	nodeStr += '}';
+	return (str += nodeStr);
+};
+
+var convertStrToTree = function (str) {
+	const inner = str.substring(1, str.length - 1);
+
+	// 첫번째 컴마 검색
+	const fci = findNextComma(0, inner);
+	// 컴마를 조사할 때 컴마가 존재하지 않는다면 스트링 길이를 반환
+	const curVal = +inner.substring(0, fci).split(':')[1];
+	const cur = new TreeNode(curVal);
+
+	// 첫번째 컴마가 없다면 자식이 없으므로 바로 리턴
+	if (inner[fci] === undefined || inner[fci] !== ',') return cur;
+
+	// 오른쪽 자식만 존재할 때
+	if (inner[fci + 1] === 'r') {
+		const rEndIdx = sliceChildNode(fci + 3, inner);
+		const r = inner.substring(fci + 3, rEndIdx + 1);
+		cur.right = convertStrToTree(r);
+		return cur;
+	}
+
+	//왼쪽 자식 탐색
+	const lEndIdx = sliceChildNode(fci + 3, inner);
+	const l = inner.substring(fci + 3, lEndIdx + 1);
+	cur.left = convertStrToTree(l);
+
+	//또다른 컴마가 존재한다면 오른쪽 자식이 존재
+	const sci = findNextComma(lEndIdx, inner);
+
+	//없다면 바로 리턴
+	if (!inner[sci] === undefined || inner[sci] !== ',') return cur;
+
+	//오른쪽 자식 탐색
+	const rEndIdx = sliceChildNode(sci + 3, inner);
+	const r = inner.substring(sci + 3, rEndIdx + 1);
+	cur.right = convertStrToTree(r);
+
+	return cur;
+};
+
+//자식의 노드를 탐색
+//탐색시 자식의 자식의 노드도 존재할 수 있으므로 스택을 이용하여 스택에 아무 값도 없을 때까지 진행
+var sliceChildNode = function (start, str) {
+	let end = start;
+	const s = [str[start]];
+	while (end < str.length) {
+		end++;
+		if (str[end] === '{') s.push(str[end]);
+		else if (str[end] === '}') {
+			if (s[s.length - 1] === '{') {
+				s.pop();
+			}
+		}
+		if (s.length === 0) break;
+	}
+	return end;
+};
+
+var findNextComma = function (i, str) {
+	let j = i;
+	while (j < str.length) {
+		if (str[j] === ',') return j;
+		j++;
+	}
+	return j + 1;
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function (data) {
+	if (data === '') return null;
+	return convertStrToTree(data);
+};
