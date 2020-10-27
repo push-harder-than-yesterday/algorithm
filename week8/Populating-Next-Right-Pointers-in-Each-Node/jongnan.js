8d6e95a99c9d629debd4c37992c2e3f9449134c3
jong yoon <dhorlawhddbs@naver.com>
2020-10-27 22:20:21 +0900

    Populating-Next-Right-Pointers-in-Each-Node 문제 완료

---
+++ week8/Populating-Next-Right-Pointers-in-Each-Node/jongnan.js
@@ -0,0 +1,33 @@
+/////////////////////////////////////////////////////////
+// 접근 방식 1
+//-------------------------------------------------------
+// BFS 방식을 통해 같은 레벨인 노드들을 구함
+// 이후, 해당 노드들을을 엮어 줌
+/////////////////////////////////////////////////////////
+var connect = function (root) {
+	if (root === null) return null;
+	const q = [[root, 0]];
+	let l = 1;
+	while (q.length !== 0) {
+		let pre = null;
+		while (q.length !== 0 && q[0][1] < l) {
+			const [node, lev] = q.shift();
+			if (node.left !== null) {
+				q.push([node.left, lev + 1]);
+			}
+			if (node.right !== null) {
+				q.push([node.right, lev + 1]);
+			}
+
+			// 노드들 엮어주기
+			if (pre === null) {
+				pre = node;
+				continue;
+			}
+			pre.next = node;
+			pre = pre.next;
+		}
+		l++;
+	}
+	return root;
+};
